1d36e820fab9effeb14493c46bc0d15af7d8713c
pfsjdsn <canoecanoe@aliyun.com>
2020-12-30 11:58:57 +0800

    Ranking List page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+import request from '../../utils/request'
 Page({
 
   /**
@@ -25,62 +25,21 @@ Page({
         iconName: 'iconzhibobofangshexiangjiguankanmianxing'
       }
     ],
+    banners: [], // 轮播图数据
+    recommendList: [], //推荐歌单
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  async onLoad() {
+    // 获取轮播图数据
+    let {banners} = await request('/banner', {type: 2})
+    this.setData({banners})
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 获取推荐歌单数据
+    let res = await request('/personalized', {limit: 10})
+    this.setData({recommendList: res.result})
   }
 })
