56bcf25bd0b4013c3da01e7b191386b509f5df7d
pfsjdsn <conoecanoe@aliyun.com>
2020-12-31 23:25:19 +0800

    login 交互

--- pages/login/login.js
+++ pages/login/login.js
@@ -4,7 +4,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    phone: '',
+    password: ''
   },
 
   /**
@@ -13,4 +14,35 @@ Page({
   onLoad: function (options) {
 
   },
+  handleInput(e) {    
+    let {type} = e.currentTarget.dataset
+    this.setData({
+      [type]: e.detail.value //变量， 自动赋值
+    })
+  },
+  login(){
+    let {phone , password} = this.data
+    if (!phone) {
+      wx.showToast({
+        title: '手机号不能为空！',
+        icon: 'none'
+      })
+      return
+    }
+    let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/ // 一位两位第三位是数字，然后是9个数字
+    if(!phoneReg.test(phone)) {
+      wx.showToast({
+        title: '手机号格式错误！',
+        icon: 'none'
+      })
+      return
+    }
+    if(!password) {
+      wx.showToast({
+        title: '密码不能为空！',
+        icon: 'none'
+      })
+      return
+    }
+  }
 })
