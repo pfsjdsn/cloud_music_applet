cf34114aad1c66cc3e922cf6b8e93b10ba3ad156
pfsjdsn <canoecanoe@aliyun.com>
2021-01-04 17:52:47 +0800

    Data report pull up page

--- pages/recommendSong/index.js
+++ pages/recommendSong/index.js
@@ -1,5 +1,20 @@
-import request from '../../utils/request'
 
+/**
+ * 消息页面通信
+ * 1. 1. 绑定事件
+      1. 事件名
+      2. 事件的回调
+      3. 订阅方: PubSub.subscribe(事件名，事件的回调)
+      4. 订阅方式接受数据的一方
+   2. 触发事件
+      1. 事件名
+      2. 提供事件参数对象， 等同于原生事件的event对象
+      3. 发布方: PubSub.publish(事件名，提供的数据)
+      4. 发布方是提供数据的一方
+ * 
+ */
+import request from '../../utils/request'
+import PubSub from 'pubsub-js'
 Page({
 
   /**
@@ -10,6 +25,7 @@ Page({
     month: '',
     day: '',
     recommendList: [], // 推荐音乐列表
+    index: 0, //点击歌曲的下标
 
   },
 
@@ -35,6 +51,26 @@ Page({
       day: new Date().getDate(),
     })
     this.getRecommendList()
+    // 订阅来自songDetail页面发布的消息
+    PubSub.subscribe('switchTpye', (msg, type) => {
+      let {recommendList, index} = this.data
+      if (type == 'pre') { // 上一首
+        // 如果是第一首，就跳转到最后一首
+        (index == 0) && (index = recommendList.length)
+        index -= 1 // 减1
+      } else { // 下一首
+        // 如果是最后一首，就跳转到第一首
+        (index == recommendList.length -1) && (index = -1)
+        index += 1
+      }
+      // 更新下标
+      this.setData({
+        index
+      })
+      let musicId = recommendList[index].id
+      // 将musicId回传给songDetail页面
+      PubSub.publish('musicId', musicId)
+    })
   },
   async getRecommendList() {
     let recommendList = await request('/recommend/songs')
@@ -45,7 +81,8 @@ Page({
   // 跳转到歌曲详情
   toSongDetail(e) {
     console.log(e);
-    let {song} = e.currentTarget.dataset
+    let {song, index} = e.currentTarget.dataset
+    this.setData({index})
     wx.navigateTo({
       url: '/pages/songDetail/index?musicId=' + song.id,
     })
--- pages/songDetail/index.js
+++ pages/songDetail/index.js
@@ -1,15 +1,28 @@
+/**
+ * 消息页面通信
+ * 1. 1. 绑定事件
+      1. 事件名
+      2. 事件的回调
+      3. 订阅方: PubSub.subscribe(事件名，事件的回调)
+      4. 订阅方式接受数据的一方
+   2. 触发事件
+      1. 事件名
+      2. 提供事件参数对象， 等同于原生事件的event对象
+      3. 发布方: PubSub.publish(事件名，提供的数据)
+      4. 发布方是提供数据的一方
+ * 
+ */
 import request from '../../utils/request'
+
+import PubSub from 'pubsub-js'
 const app = getApp()
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     isPlay: false, // 音乐是否在播放
     song: {}, //音乐详情
     musicId: '', //音乐id
+    musicLink: '', // 音乐链接
   },
 
   /**
@@ -40,7 +53,7 @@ Page({
     this.backgroundAudioManager.onStop(() => {
       this.changePlayState(false)
     })
-  },
+},
   // 修改播放状态封装的函数
   changePlayState(isPlay) {
     // 修改音乐播放的状态
@@ -52,10 +65,9 @@ Page({
   },
   //点击播放
   handleMusicPlay() {
-    this.setData({
-      isPlay: !this.data.isPlay
-    })
-    this.musicConrol(this.data.isPlay,this.data.musicId)
+    let isPlay = !this.data.isPlay
+    let {musicId, musicLink}  = this.data
+    this.musicConrol(isPlay, musicId, musicLink)
 
   },
   // 获取音乐详情
@@ -70,18 +82,36 @@ Page({
     })
   },
   // 控制音乐播放/暂停
-  async musicConrol(isPlay, musicId){
+  async musicConrol(isPlay, musicId, musicLink){
     if (isPlay) {
-      // 获取音乐链接
-      let musicLinkData = await request('/song/url', {id: musicId})
-      this.backgroundAudioManager.src = musicLinkData.data[0].url
+      if (!musicLink) { // 避免重复发获取音乐链接的请求
+        // 获取音乐链接
+        let musicLinkData = await request('/song/url', {id: musicId})
+        musicLink = musicLinkData.data[0].url 
+        this.setData({musicLink})
+      }
+      this.backgroundAudioManager.src = musicLink
       this.backgroundAudioManager.title = this.data.song.name
     } else  { // 暂停音乐
       this.backgroundAudioManager.pause()
     }
   },
   // 切换上一首或下一首歌
-  handleSwitch() {
-
+  handleSwitch(e) {
+    let {type} = e.currentTarget.dataset
+    // 关闭当前播放的音乐
+    this.backgroundAudioManager.stop()
+    // 订阅来自recommendSong页面发布的musicId的消息(onload页面中，只会执行一次 )
+    PubSub.subscribe('musicId', (msg, musicId) => {
+      console.log(musicId);
+      // 获取音乐信息
+      this.getMusicInfo(musicId)
+      // 自动播放
+      this.musicConrol(true, musicId)
+      // 取消订阅
+      PubSub.unsubscribe('musicId')
+    })
+    // 发布消息数据给recommendSong页面
+    PubSub.publish('switchTpye', type)
   },
 })
